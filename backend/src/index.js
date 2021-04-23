e260055ede9df7d83011c58db897b875962c62a4
Jan André Greschner <a01634939@unet.univie.ac.at>
2021-04-23 13:15:41 +0200

    added linter

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,26 @@
-const name1 = 'James';
+// created with lots of love by Jan André Greschner
+import express from 'express';
+import createError from 'http-errors';
+import logger from './logger.js';
 
-const person = { first: name1 };
+// set listening port
+const PORT = process.env.PORT || 4000;
 
-console.log(person);
+const app = express();
+app.use(express.json());
 
-const sayHelloLinting = (fName) => {
-  console.log(`Hello linting, ${fName}`);
-};
+// catch 404 and forward to error handler
+app.use((_req, _res, next) => {
+  next(createError(404));
+});
+
+// error handler
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ error: err.message });
+});
+
+// listen on port
+app.listen(PORT, () => {
+  logger.info(`Server is listening on port: ${PORT}`);
+});

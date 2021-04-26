4e33cfefb244e92640869e51250f762f48bd39fb
Jan André Greschner <a01634939@unet.univie.ac.at>
2021-04-26 18:02:23 +0200

    added body parser

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use('/', receiveRoutes);

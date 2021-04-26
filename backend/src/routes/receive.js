141de33fe1aa7fca83d57cd9b2d8310b2c5ffa97
Jan André Greschner <a01634939@unet.univie.ac.at>
2021-04-26 17:51:50 +0200

    test

--- backend/src/routes/receive.js
+++ backend/src/routes/receive.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.post('/receive', (req, res, next) => {
   console.log(req.headers);
+  console.log(req.query);
   console.log(req.body);
   res.set('CPEE-CALLBACK', 'true').sendStatus(200);
 });

cee1b8f207d19b3bf4ab6041397a5282d70000ce
Jan André Greschner <a01634939@unet.univie.ac.at>
2021-05-03 11:29:11 +0200

    a

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,7 @@ const io = new Server(server, {
 
 io.on('connection', (socket) => {
   logger.info('connected');
-  io.set('match origin protocol', true);
+  io.set('transports', ['websocket']);
   socket.on('disconnect', () => {
     logger.info('client disconnected');
   });

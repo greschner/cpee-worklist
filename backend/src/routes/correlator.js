dc588ca6fab3a156f20de7ba168fd763d50cb404
Jan André Greschner <jan.greschner@me.com>
2022-05-26 19:00:37 +0200

    performance improvement

--- backend/src/routes/correlator.js
+++ backend/src/routes/correlator.js
@@ -36,55 +36,8 @@ const matchTask = (pid, body) => {
   }
 };
 
-router.post('/', schemaValidation(taskSchema.POST, 'body'), (req, res, next) => {
-  try {
-    // const xml = await readFile('/Users/jangreschner/dockerProjects/labMaster/logging/test_sub.xml', { encoding: 'utf8' });
-    const { stop } = req.body;
-    // cpee callback request
-    if (req.headers['cpee-callback'] && !stop) {
-      const { pid, ...body } = req.body;
-      taskModel.create({
-        label: req.headers['cpee-label'],
-        pid,
-        activity: req.headers['cpee-activity'],
-        callback: req.headers['cpee-callback'],
-        callbackId: req.headers['cpee-callback-id'],
-        instance: req.headers['cpee-instance'],
-        instanceUuid: req.headers['cpee-instance-uuid'],
-        instanceUrl: req.headers['cpee-instance-url'],
-        body,
-      }).then((task) => {
-        logger.info(`New Task created: ${task}`);
-        sendEventsToAll(task, 'add'); // sse
-      }); // save new task to db
-      res.setHeader('CPEE-CALLBACK', 'true');
-    }
-
-    // producer
-    if (req.headers['content-id'] === 'producer') {
-      producedModel.create(req.body).then((t) => {
-        logger.info(`New produced Task created: ${t}`);
-      }); // save new produced entry to db
-    }
-
-    // delete task
-    if (stop) {
-      taskModel.findOneAndDelete({ pid: req.body.pid, instance: req.headers['cpee-instance'] }).then((task) => {
-        if (task) {
-          logger.info(`New produced delete Task created: ${task}`);
-          callbackInstance(task.callback, 'nil', { 'Content-Type': 'text/plain' });
-        }
-      });
-    }
-  } catch (error) {
-    next(error);
-  }
-  res.status(200).send();
-  next();
-});
-
 // correlator
-router.all('/', () => {
+const correlator = () => {
   try {
     taskModel.find({}).then((openTasks) => {
       Promise.all(openTasks.map(async ({
@@ -117,7 +70,94 @@ router.all('/', () => {
   } catch (error) {
     console.error(error);
   }
+};
+
+router.post('/', schemaValidation(taskSchema.POST, 'body'), (req, res, next) => {
+  try {
+    // eslint-disable-next-line max-len
+    // const xml = await readFile('/Users/jangreschner/dockerProjects/labMaster/logging/test_sub.xml', { encoding: 'utf8' });
+
+    const { stop } = req.body;
+
+    // cpee callback request
+    if (req.headers['cpee-callback'] && !stop) {
+      const { pid, ...body } = req.body;
+      taskModel.create({
+        label: req.headers['cpee-label'],
+        pid,
+        activity: req.headers['cpee-activity'],
+        callback: req.headers['cpee-callback'],
+        callbackId: req.headers['cpee-callback-id'],
+        instance: req.headers['cpee-instance'],
+        instanceUuid: req.headers['cpee-instance-uuid'],
+        instanceUrl: req.headers['cpee-instance-url'],
+        body,
+      }).then((task) => {
+        logger.info(`New Task created: ${task}`);
+        sendEventsToAll(task, 'add'); // sse
+        correlator();
+      }); // save new task to db
+      res.setHeader('CPEE-CALLBACK', 'true');
+    }
+
+    // producer
+    if (req.headers['content-id'] === 'producer') {
+      producedModel.create(req.body).then((t) => {
+        logger.info(`New produced Task created: ${t}`);
+        correlator();
+      }); // save new produced entry to db
+    }
+
+    // delete task
+    if (stop) {
+      taskModel.findOneAndDelete({ pid: req.body.pid, instance: req.headers['cpee-instance'] }).then((task) => {
+        if (task) {
+          logger.info(`New produced delete Task created: ${task}`);
+          callbackInstance(task.callback, 'nil', { 'Content-Type': 'text/plain' });
+        }
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+  res.status(200).send();
 });
+
+// correlator
+/* router.all('/', () => {
+  try {
+    taskModel.find({}).then((openTasks) => {
+      Promise.all(openTasks.map(async ({
+        pid, callback, _id: id, body, label, instance,
+      }) => {
+        matchTask(pid, body).then((producedTask) => {
+          if (producedTask) {
+            logger.info(`MATCH Task: ${{
+              id, label, pid, instance, body,
+            }} with ${producedTask}`);
+
+            const cArr = ['1', '2'].includes(pid);
+
+            Promise.all([
+              callbackInstance(callback, {
+                ...producedTask.body,
+                timestamp: producedTask.timestamp,
+              }, cArr && { 'cpee-update': true }), // callback to CPEE
+              ...!cArr ? [taskModel.findByIdAndDelete(id)] : [], // remove from task list
+              ...pid !== '6' ? [
+                producedModel.findByIdAndDelete(producedTask._id),
+              ] : [], // remove from produced list
+            ]);
+
+            sendEventsToAll(id, 'remove'); // sse
+          }
+        }); // match
+      }));
+    }); // get all open tasks
+  } catch (error) {
+    console.error(error);
+  }
+}); */
 
 /* router.post('/t', schemaValidation(taskSchema.POST, 'body'), (req, res) => {
   console.log(req.body);

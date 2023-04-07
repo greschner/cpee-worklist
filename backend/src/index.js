f107f0e083661265bff5c980b811c8bc3082a7e8
Jan André Greschner <jan.greschner@me.com>
2023-04-07 17:25:53 +0200

    revert

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,10 +16,10 @@ server.listen(PORT, () => {
 });
 
 // app insights
-appInsights
+/* appInsights
   .setup('InstrumentationKey=68e567a2-444f-4b51-8d27-cb0c6e52c791;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/')
   .start()
-  .setSendLiveMetrics(true);
+  .setSendLiveMetrics(true); */
 
 const gracefulStop = (signal) => {
   logger.info(`${signal} signal received. Shutting down...`);

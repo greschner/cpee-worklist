07cc88734c27afeb71987e688e6bb787272ac0a9
Jan André Greschner <jan.greschner@me.com>
2023-04-07 18:10:22 +0200

    test app insights

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 // created with lots of ❤️ by Jan André Greschner
+import appInsights from 'applicationinsights';
 import db from './db.js';
 import logger from './logger.js';
 import { server, io } from './socket.js';
@@ -15,10 +16,10 @@ server.listen(PORT, () => {
 });
 
 // app insights
-/* appInsights
+appInsights
   .setup('InstrumentationKey=68e567a2-444f-4b51-8d27-cb0c6e52c791;IngestionEndpoint=https://northeurope-2.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/')
   .start()
-  .setSendLiveMetrics(true); */
+  .setSendLiveMetrics(true);
 
 const gracefulStop = (signal) => {
   logger.info(`${signal} signal received. Shutting down...`);
